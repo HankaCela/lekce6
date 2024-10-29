c4f796000ce2347832c6f1b07a6487d3d3ac570f
Hanka Celá <hana-cela@email.cz>
2024-10-29 18:26:03 +0100

    zmeny

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(validator)
+/*console.log(validator)
 console.log(validator.isEmail("pepik@seznam.cz"))
 console.log(validator.isMobilePhone("111 222 333", "cs-CZ"))
 console.log(validator.isMobilePhone("11122233", "cs-CZ"))
@@ -12,3 +12,21 @@ const cas2 = dayjs()
 console.log(cas2)
 console.log(cas2.format("DD-MM-YY"))
 console.log(cas2.format("DD/MM/YY"))
+
+HTML 
+    <script src="https://unpkg.com/validator@latest/validator.min.js"></script>
+    <script src="https://unpkg.com/dayjs@latest/dayjs.min.js"></script>
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+*/
+
+const ahoj = (jmeno) => {
+    concole.log("Ahoj uživateli ${jmeno}")
+}
+
+ahoj("Michal")
+ahoj("Jana")
+ahoj("Erik")
+ahoj("Lucie")
+
+console.log("text")
